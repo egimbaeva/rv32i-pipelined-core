ef102d4753eb87a6f4b17f46464d5cb066e8a29b
egimbaeva <egimbaeva_b@icloud.com>
2026-02-05 20:34:34 +0200

    Update pc.v

--- rtl/pc.v
+++ rtl/pc.v
@@ -1,1 +1,13 @@
-
+module pc (
+  input wire clk,
+  input wire reset,
+  input wire [31:0] pc_next,
+  output wire [31:0] pc_current
+);
+  always @(posedge clk) begin
+    if (reset)
+      pc_current <= 32'b0;
+    else 
+      pc_current <= pc_next;
+  end
+endmodule
